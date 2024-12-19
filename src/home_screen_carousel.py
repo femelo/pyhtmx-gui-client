ff56a7e32192c9ef3646b6d6daa4a617687bc6cf
Flávio Eler De Melo <flavio.eler@gmail.com>
2024-12-19 15:44:18 +0100

    Update carousel style

--- src/home_screen_carousel.py
+++ src/home_screen_carousel.py
@@ -1,11 +1,8 @@
 from __future__ import annotations
-import shutil
 import os
 from typing import Any, Optional, List, Dict
-from pyhtmx.html_tag import HTMLTag
 from pyhtmx import Div, Img, Script, Link
-from kit import SessionItem, Trigger, Control, WidgetType, Widget, Page 
-
+from kit import SessionItem, Widget, Page
 
 
 # Weather icon mapping
@@ -45,7 +42,10 @@ class ClockTimeWidget(Widget):
         )
         self._widget: Div = Div(
             [
-                Div(inner_content="Current Time", _class="modern-title"),
+                Div(
+                    inner_content="Current Time",
+                    _class="modern-title"
+                ),
                 time_text,
             ],
         )
@@ -95,7 +95,12 @@ class DateTimeWidget(Widget):
         )
         # Same object for all date parameters:
         # whenever one of them changes, the object state changes
-        for parameter in ["weekday_string", "month_string", "day_string", "year_string"]:
+        for parameter in [
+            "weekday_string",
+            "month_string",
+            "day_string",
+            "year_string",
+        ]:
             self.add(parameter, session_item)
 
         # Time and date container
@@ -119,7 +124,6 @@ class DateTimeWidget(Widget):
         day = self._session_data.get("day_string", '')
         year = self._session_data.get("year_string", '')
         return f"{weekday} {month} {day}, {year}"
-
 
 
 class WeatherWidget(Widget):
@@ -158,7 +162,12 @@ class WeatherWidget(Widget):
         weather_temp_text: Div = Div(
             inner_content=self.weather_temperature(),
             _id="weather_temp",
-            _class="text-[4vw] leading-[8vw] text-white font-bold",
+            _class=[
+                "text-[4vw]",
+                "leading-[8vw]",
+                "text-white",
+                "font-bold",
+            ],
         )
         self.add(
             "weather_temp",
@@ -183,7 +192,12 @@ class WeatherWidget(Widget):
                     "gap-[2vw]",
                 ],
             ),
-            _class="flex grow justify-end items-start",
+            _class=[
+                "flex",
+                "grow",
+                "justify-end",
+                "items-start",
+            ],
         )
 
     def weather_icon_src(self: WeatherWidget, value: Any) -> str:
@@ -241,7 +255,8 @@ class HomeScreen(Page):
         self.date_time = DateTimeWidget(session_data)
         self.weather1 = WeatherWidget(session_data)
         self.weather2 = WeatherWidget(session_data)
-        self.skill_examples = SkillExamplesWidget(session_data)  # Add SkillExamplesWidget
+        # Add SkillExamplesWidget
+        self.skill_examples = SkillExamplesWidget(session_data)
         self.add(
             [
                 self.clock_time,
@@ -258,7 +273,16 @@ class HomeScreen(Page):
         # Carousel-container
         carousel_container = Div(
             carousel_items,
-            _class="carousel w-full h-full relative snap-x snap-mandatory overflow-x-auto flex",
+            _class=[
+                "carousel",
+                "w-full",
+                "h-full",
+                "relative",
+                "snap-x",
+                "snap-mandatory",
+                "overflow-x-auto",
+                "flex",
+            ],
             _id="carousel",
         )
 
@@ -270,16 +294,30 @@ class HomeScreen(Page):
                 Div(inner_content="3", _class="tab tab-lifted"),
                 Div(inner_content="4", _class="tab tab-lifted"),
             ],
-            _class="tabs tabs-boxed mb-4 w-full max-w-[15%] flex justify-center",
-            _id="tabs-container", 
+            _class=[
+                "tabs",
+                "tabs-boxed",
+                "mb-4",
+                "w-full",
+                "max-w-[15%]",
+                "flex",
+                "justify-center",
+            ],
+            _id="tabs-container",
         )
 
-        # Combine carousel and tabs 
+        # Combine carousel and tabs
         main_view = Div(
             [carousel_container, tabs_container],
             _class="h-full w-full flex flex-col items-center justify-center",
             _id="carousel-bg",
-            style="background: linear-gradient(to right, rgb(59, 130, 246), rgb(255, 182, 193)); transition: background 0.5s ease;",
+            style={
+                "background": (
+                    "linear-gradient(to right, "
+                    "rgb(59, 130, 246), rgb(255, 182, 193))"
+                ),
+                "transition": "background 0.5s ease",
+            },
         )
 
         self._page: Div = Div(
@@ -288,12 +326,12 @@ class HomeScreen(Page):
             _class="flex flex-col",
             style={"width": "100vw", "height": "100vh"},
         )
-        
+
         script_tag = Script(src="assets/js/carousel.js")
         self._page.add_child(script_tag)
-        
+
         # extra CSS for carousel
-        style_tag = Link(rel="stylesheet", href="assets/css/styles.css") 
+        style_tag = Link(rel="stylesheet", href="assets/css/styles.css")
         self._page.add_child(style_tag)
 
     def generate_carousel(self: HomeScreen) -> List[Div]:
@@ -315,19 +353,25 @@ class HomeScreen(Page):
             "text-center",
             "shadow-lg",
         ]
+        text_classes = ["modern-text", "text-lg", "mt-4"]
         # Carousel Item 1
         item1: Div = Div(
             Div(
                 [
                     Div(
                         self.date_time.widget,
-                        _class="flex flex-col items-center space-y-2",
+                        _class=[
+                            "flex",
+                            "flex-col",
+                            "items-center",
+                            "space-y-2"
+                        ],
                     ),
                     Div(
                         self.weather1.widget,
                     ),
                 ],
-                _id="full-screen-image", 
+                _id="full-screen-image",
                 _class=[
                     "full-screen-image",
                     "flex",
@@ -344,19 +388,30 @@ class HomeScreen(Page):
                 [
                     Div(
                         self.clock_time,
-                        _class=[*subitem_classes, "bg-opacity-60", "w-full"],
+                        _class=[
+                            *subitem_classes,
+                            "bg-opacity-60",
+                            "w-full",
+                        ],
                     ),
                     Div(
                         [
                             Div(
                                 [
-                                    Div(inner_content="Weather", _class="modern-title"),
+                                    Div(
+                                        inner_content="Weather",
+                                        _class="modern-title"
+                                    ),
                                     Div(
                                         self.weather2.widget,
-                                        _class="weather-widget-small", 
+                                        _class="weather-widget-small",
                                     ),
                                 ],
-                                _class=[*subitem_classes, "bg-opacity-60", "w-1/2"],
+                                _class=[
+                                    *subitem_classes,
+                                    "bg-opacity-60",
+                                    "w-1/2",
+                                ],
                             ),
                             Div(
                                 [
@@ -365,17 +420,37 @@ class HomeScreen(Page):
                                         _class="modern-title"
                                     ),
                                     Div(
-                                        inner_content="Set an alarm to wake up on time",
-                                        _class="modern-text text-lg mt-4"
+                                        inner_content=(
+                                            "Set an alarm to wake "
+                                            "up on time"
+                                        ),
+                                        _class=text_classes,
                                     ),
                                 ],
-                                _class=[*subitem_classes, "bg-opacity-60", "w-1/2"],
+                                _class=[
+                                    *subitem_classes,
+                                    "bg-opacity-60",
+                                    "w-1/2",
+                                ],
                             ),
                         ],
-                        _class="flex w-full justify-between space-x-4",
+                        _class=[
+                            "flex",
+                            "w-full",
+                            "justify-between",
+                            "space-x-4",
+                        ],
                     ),
                 ],
-                _class="flex flex-col items-center space-y-4 w-full max-w-4xl px-4",
+                _class=[
+                    "flex",
+                    "flex-col",
+                    "items-center",
+                    "space-y-4",
+                    "w-full",
+                    "max-w-4xl",
+                    "px-4",
+                ],
             ),
             _class=item_classes,
         )
@@ -390,8 +465,11 @@ class HomeScreen(Page):
                                 _class="modern-title",
                             ),
                             Div(
-                                inner_content="Stay updated with the latest events happening around the world.",
-                                _class="modern-text text-lg mt-4",
+                                inner_content=(
+                                    "Stay updated with the latest events "
+                                    "happening around the world."
+                                ),
+                                _class=text_classes,
                             ),
                         ],
                         _class=[*subitem_classes, "bg-opacity-40"],
@@ -403,8 +481,11 @@ class HomeScreen(Page):
                                 _class="modern-title",
                             ),
                             Div(
-                                inner_content="Listen to your favorite playlists and artists on Spotify.",
-                                _class="modern-text text-lg mt-4",
+                                inner_content=(
+                                    "Listen to your favorite playlists "
+                                    "and artists on Spotify."
+                                ),
+                                _class=text_classes,
                             ),
                         ],
                         _class=[*subitem_classes, "bg-opacity-40"],
@@ -416,8 +497,11 @@ class HomeScreen(Page):
                                 _class="modern-title",
                             ),
                             Div(
-                                inner_content="Enjoy a lighthearted joke to brighten your day.",
-                                _class="modern-text text-lg mt-4",
+                                inner_content=(
+                                    "Enjoy a lighthearted joke "
+                                    "to brighten your day."
+                                ),
+                                _class=text_classes,
                             ),
                         ],
                         _class=[*subitem_classes, "bg-opacity-40"],
@@ -429,8 +513,11 @@ class HomeScreen(Page):
                                 _class="modern-title",
                             ),
                             Div(
-                                inner_content="Dive into an exciting underwater adventure game.",
-                                _class="modern-text text-lg mt-4",
+                                inner_content=(
+                                    "Dive into an exciting underwater "
+                                    "adventure game."
+                                ),
+                                _class=text_classes,
                             ),
                         ],
                         _class=[*subitem_classes, "bg-opacity-40"],
@@ -442,7 +529,8 @@ class HomeScreen(Page):
                                 inner_content="Skill examples",
                                 _class="modern-title"
                             ),  # Add title "Skill examples"
-                            self.skill_examples.widget,  # Add SkillExamplesWidget
+                            # Add SkillExamplesWidget
+                            self.skill_examples.widget,
                         ],
                         _class=[*subitem_classes, "bg-opacity-40"],
                     ),
@@ -456,7 +544,7 @@ class HomeScreen(Page):
                     "px-4",
                 ],
             ),
-            _class=item_classes, 
+            _class=item_classes,
         )
         # Carousel Item 4
         item4: Div = Div(
@@ -473,6 +561,6 @@ class HomeScreen(Page):
                 ],
                 _class=subitem_classes,
             ),
-            _class=item_classes, 
+            _class=item_classes,
         )
         return [item1, item2, item3, item4]
