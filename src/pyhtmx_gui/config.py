768637e01d2b5cd0adc6cf77437dfa4a5f5f90ac
Flávio Eler De Melo <flavio.eler@gmail.com>
2024-12-21 12:15:41 +0100

    Add default config in packaged data

--- src/pyhtmx_gui/config.py
+++ src/pyhtmx_gui/config.py
@@ -7,5 +7,5 @@ else:
 
 APP_DIR = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(APP_DIR, "config.toml"), "rb") as f:
+with open(os.path.join(APP_DIR, "config", "config.toml"), "rb") as f:
     config_data = tomllib.load(f)
