3fa858c27165ccf216dab74e86c2a0390d96a09c
Flávio Eler De Melo <flavio.eler@gmail.com>
2024-12-26 18:11:13 +0100

    Add lottie player

--- src/pyhtmx_gui/master.py
+++ src/pyhtmx_gui/master.py
@@ -32,12 +32,32 @@ MASTER_DOCUMENT: Html = Html(
                     rel="stylesheet",
                     _type="text/css",
                 ),
-                Script(src="./assets/js/tailwind-play-cdn.js"),
-                Script(src="./assets/js/htmx.min.js", defer="true"),
-                Script(src="./assets/js/sse.js", defer="true"),
-                Script(src="./assets/js/htmx-process.js", defer="true"),
+                Script(
+                    src="./assets/js/tailwind-play-cdn.js",
+                    _type="text/javascript",
+                ),
+                Script(
+                    src="./assets/js/htmx.min.js",
+                    _type="text/javascript",
+                    defer="true",
+                ),
+                Script(
+                    src="./assets/js/sse.js",
+                    _type="text/javascript",
+                    defer="true",
+                ),
+                Script(
+                    src="./assets/js/htmx-process.js",
+                    _type="text/javascript",
+                    defer="true",
+                ),
+                Script(
+                    src="./assets/js/lottie-player.js",
+                    _type="text/javascript",
+                    defer="true",
+                ),
                 Title("PyHTMX GUI Client"),
-            ]
+            ],
         ),
         Body(
             Div(
